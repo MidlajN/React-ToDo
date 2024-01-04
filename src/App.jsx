21ab471990ee0aa1971ceb7b1511c8b074e8fc48
Muhammed Midlaj <midlaj4n@gmail.com>
2024-01-05 00:14:59 +0530

    React-ToDo : Todo App Initial MVP

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date());
-  const [toDos, setToDos] = useState([]);
+  const [toDos, setToDos] = useState([])
   const [toDo, setToDo] = useState('')
 
   useEffect(() => {
@@ -32,24 +32,49 @@ function App() {
               <div className='todoDiv' style={{ alignItems:'flex-end', paddingBlock: '20px 25px' }}>
                 <input value={toDo} onChange={(e)=>{setToDo(e.target.value)}} className='toDoInput' type="text" />
                 <div style={{padding: '10px 15px', background: 'linear-gradient(0deg, rgb(22 56 87) 0%, rgba(11,91,154,0) 55%)'}}>
-                  <i onClick={()=>{setToDos(toDos =>[...toDos, {text: toDo, status: false, id: Date.now()}], console.log(toDos))}} className="fa-solid fa-calendar-plus todoBtn"></i>
+                  <i onClick={()=>{setToDos([...toDos, {text: toDo, status: false, id: Date.now()}])}} className="fa-solid fa-calendar-plus todoBtn"></i>
                 </div>
               </div>
             </div>
             {
               toDos.map((object) =>{
-                return (
-                  <div className="todoList">
-                    <input type="checkbox" value={object.status} name="status" id="status" />
-                    <p>{object.text}</p>
-                    <i className="fa-regular fa-circle-xmark fa-lg"></i>
-                  </div>
-                )
+                const time = new Date(object.id)
+                if (object.status == false){
+                  return (
+                    <div className="todoList" key={object.id}>
+                      <input onChange={(e) =>{
+                        setToDos(toDos.filter(obj => {
+                          if (obj.id === object.id){
+                            obj.status = e.target.checked;
+                            console.log('NEW::::', obj)
+                          }
+                          return obj
+                        })  )
+                      }} type="checkbox" value={object.status}  name="status" id={object.id} />
+                      <p>{object.text}</p>
+                      <span>Created On : {time.toLocaleString()}</span>
+                      <i className="fa-regular fa-circle-xmark fa-lg"></i>
+                    </div>
+                  )
+                }
               })
             }
           </div>
-          <div>
-            <h1>New Area</h1>
+          <div className='todoDone' >
+          {
+            toDos.map((object) => {
+              if (object.status){
+                const time = new Date(object.id).toLocaleString();
+                return (
+                  <div className="todoList" key={object.id}>
+                    <p>{object.text}</p>
+                    <span>Created On : {time}</span>
+                    <i className="fa-regular fa-circle-xmark fa-lg"></i>
+                  </div> 
+                )
+              }
+            })
+          }
           </div>
         </div>
       </div>
